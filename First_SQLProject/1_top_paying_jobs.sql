42655f019a97bf7f6344d2c5664bb5b58cd2da2b
Nitin Prajapati <143989538+nitinprajapati888@users.noreply.github.com>
2025-06-16 17:29:10 +0530

    Top 10 paying company for Data Analyst job roll

---
+++ First_SQLProject/1_top_paying_jobs.sql
@@ -0,0 +1,19 @@
+-- Here is a query for top 10 paying company for Data Analyst job roll
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM 
+    job_postings_fact
+LEFT JOIN company_dim ON company_dim.company_id = job_postings_fact.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
